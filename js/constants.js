d24e73690f8644a213ee1ec1f9ba8ff7766ee21c
Adam Gincel <adam.gincel@gmail.com>
2018-12-03 20:59:47 -0500

    Added /downb (minnesota)

--- js/constants.js
+++ js/constants.js
@@ -58,6 +58,7 @@ function enableNavbarBurgers() {
 
 
 dataMap["Ori (Michigan TO)"] = "1-3-44-37-40h_42h-20-36h-79h-85-33h";
+dataMap["DownB (Minnesota Smash)"] = "1h-3h-44h-37h-40h_42h-79h-85h";
 dataMap["2GG"] = "1-3-44-24h-20_85-39h-42h-62h-33h-36h-77h-38h-17h-37";
 dataMap["ZeRo"] = "1-3-20-40h-44_85-17h-37-36h-42h-39h";
 dataMap["Ottawa"] = "1-3-24h-44-37h-39h-79h_19-20-17h-33h-36h-38h-42h-62h-66h-77h-85";
